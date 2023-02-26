45927898a85614a5c5c8fc043a39d1fc3469d4d9
devnahidislam <mdnahidislam30.com>
2023-02-26 20:48:52 +0600

    User page designed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,51 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  PageNotFound,
+  Password,
+  Profile,
+  Recovery,
+  Register,
+  Reset,
+  Username,
+} from "./components";
+
+// Root Route
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h2>React Login App</h2>
-    </div>
+    <main className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
---
+++ client/src/components/Username.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from "../styles/Username.module.css";
+import toast, { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { usernameValidate } from "../helper/validate";
+
+const Username = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validate: usernameValidate,
+    validateOnBlure: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <span className="py-1 text-xl text-gray-500">
+              Explore more by connecting with us
+            </span>
+          </div>
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-3">
+              <img className={styles.profile_img} src={avatar} alt="avatar" />
+            </div>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                {...formik.getFieldProps("username")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button
+                className="border bg-indigo-500 w-3/4 py-2 rounded-lg text-gray-50 text-xl shadow-sm text-center hover:bg-indigo-800"
+                type="submit"
+              >
+                Let's Go
+              </button>
+            </div>
+            <div className="text-center mt-2">
+              <span className="text-gray-500">
+                Not a member?{" "}
+                <Link
+                  className="text-blue-500 hover:text-blue-800"
+                  to="/register"
+                >
+                  Register Now
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Username;
